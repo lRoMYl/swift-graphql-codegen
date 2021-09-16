c8550f11edf86ee27d8100e859c4f146675f909c
Romy <romeo32121@hotmail.com>
2021-09-16 13:59:44 +0800

    Remove version

--- Formula/dh-graphql-codegen-ios-test.rb
+++ Formula/dh-graphql-codegen-ios-test.rb
@@ -4,7 +4,6 @@ class DhGraphqlCodegenIosTest < Formula
   url "https://github.com/lRoMYl/homebrew-test-brew-cli/archive/0.1.4.tar.gz"
   sha256 "2ef751ea8b0bba39cbd003e4ef70a33841b23ca2dbd35064b142a540919c448a"
   head "https://github.com/lRoMYl/homebrew-test-brew-cli.git"
-  version "0.1.3"
 
   depends_on :xcode => :build
   depends_on :macos
