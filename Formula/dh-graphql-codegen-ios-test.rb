f769273931535ae45a66ae763f868ed5c81296b5
Romy <romeo32121@hotmail.com>
2021-09-16 16:06:22 +0800

    Updated sha

--- Formula/dh-graphql-codegen-ios-test.rb
+++ Formula/dh-graphql-codegen-ios-test.rb
@@ -2,7 +2,7 @@ class DhGraphqlCodegenIosTest < Formula
   desc "Swift GraphQL Code Generator"
   homepage "https://github.com/lRoMYl/homebrew-test-brew-cli"
   url "https://github.com/lRoMYl/homebrew-test-brew-cli/archive/0.1.10.tar.gz"
-  sha256 "5fe9ea0801f1332837a69a2a74519f68190d52ab161c5154f327a248b7d78420"
+  sha256 "161f76bdbed6447d8f1c152ed5768658b99f9fee9d993e429a8c75bb63c38b7e"
   head "https://github.com/lRoMYl/homebrew-test-brew-cli.git"
 
   depends_on :xcode => :build
