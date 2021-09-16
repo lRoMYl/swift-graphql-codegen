a77843d712b39134a443902e030eb44b6873381b
Romy <romeo32121@hotmail.com>
2021-09-16 16:34:39 +0800

    Remove homebrew prefix

--- Formula/dh-graphql-codegen-ios-test.rb
+++ Formula/dh-graphql-codegen-ios-test.rb
@@ -1,9 +1,9 @@
 class DhGraphqlCodegenIosTest < Formula
   desc "Swift GraphQL Code Generator"
-  homepage "https://github.com/lRoMYl/homebrew-test-brew-cli"
-  url "https://github.com/lRoMYl/homebrew-test-brew-cli/archive/0.1.10.tar.gz"
+  homepage "https://github.com/lRoMYl/test-brew-cli"
+  url "https://github.com/lRoMYl/test-brew-cli/archive/0.1.10.tar.gz"
   sha256 "161f76bdbed6447d8f1c152ed5768658b99f9fee9d993e429a8c75bb63c38b7e"
-  head "https://github.com/lRoMYl/homebrew-test-brew-cli.git"
+  head "https://github.com/lRoMYl/test-brew-cli.git"
 
   depends_on :xcode => :build
   depends_on :macos
