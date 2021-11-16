98a84a65ec26d288e87cdc1468d9e98fbd089947
Romy <romeo32121@hotmail.com>
2021-11-16 15:10:02 +0800

    Added test for InputObjectSpecGeneratorTests

---
+++ Tests/GraphQLCodegenSpecSwiftTests/Generators/InputObjectSpecGeneratorTests.swift
@@ -0,0 +1,55 @@
+//
+//  File.swift
+//  
+//
+//  Created by Romy Cheah on 16/11/21.
+//
+
+@testable import GraphQLAST
+@testable import GraphQLCodegenConfig
+@testable import GraphQLCodegenNameSwift
+@testable import GraphQLCodegenSpecSwift
+import XCTest
+
+final class InputObjectSpecGeneratorTests: XCTestCase {
+  func test() throws {
+    let generator = InputObjectCodeGenerator(
+      scalarMap: .default,
+      entityNameMap: .default,
+      entityNameProvider: DHEntityNameProvider(scalarMap: .default, entityNameMap: .default)
+    )
+
+    let starWarsSchema = try Schema.schema(from: "StarWarsTestSchema")
+
+    let declaration = try generator.code(schema: starWarsSchema).format()
+    let expectedDeclaration = try """
+    // MARK: - Input Objects
+
+    struct GreetingGraphQLInputObject: Codable {
+
+      let language: LanguageGraphQLEnumObject?
+
+      let name: String
+
+      // MARK: - CodingKeys
+
+      private enum CodingKeys: String, CodingKey {
+        case language = "language"
+        case name = "name"
+      }
+    }
+
+    struct GreetingOptionsGraphQLInputObject: Codable {
+      let prefix: String?
+
+      // MARK: - CodingKeys
+
+      private enum CodingKeys: String, CodingKey {
+        case prefix = "prefix"
+      }
+    }
+    """.format()
+
+    XCTAssertEqual(declaration, expectedDeclaration)
+  }
+}
